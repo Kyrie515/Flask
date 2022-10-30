4bfa1ed333c5bda3fb837b856e40b5c96eed6803
ubuntu <ubuntu@localhost.localdomain>
2022-10-30 10:23:17 +0800

    Add some functions to auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,6 +1,6 @@
 import functools
 
-from flask import Blueprint, g, current_app, request
+from flask import Blueprint, g, current_app, request, redirect, url_for, flash, render_template, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
@@ -27,4 +27,65 @@ def register():
                 )
             except db.IntegrityError:
                 error = f"User {username} already exists!"
-            
+            else:
+                return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', method=('GET', 'POST'))
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        #function fetchone() returns the result as a dict
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?",
+            (username)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username"
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password"
+        #session is a dict that stores data across requests. When validation succeeds, the user’s id is stored in a new session. The data is stored in a cookie that is sent to the browser, and the browser then sends it back with subsequent requests. Flask securely signs the data so that it can’t be tampered with
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+        return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view): #This is a decorator
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        else:
+            return view(**kwargs)
+
+    return wrapped_view
+
+
+
+
