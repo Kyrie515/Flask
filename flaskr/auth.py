5646b2b4d8618acea3f3f013c06b97bafa7928f9
ubuntu <ubuntu@localhost.localdomain>
2022-10-31 20:23:01 +0800

    Complete the auth part

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -34,7 +34,7 @@ def register():
 
     return render_template('auth/register.html')
 
-@bp.route('/login', method=('GET', 'POST'))
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == "POST":
         username = request.form['username']
